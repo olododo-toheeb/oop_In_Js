404bc706105f65f7e93ddd4a63a29710e597518e
olododo-toheeb <115680174+olododo-toheeb@users.noreply.github.com>
2023-01-23 11:58:58 +0100

    make small change

--- index.js
+++ index.js
@@ -12,3 +12,15 @@ const circle = createCircle(1);
 circle.draw();
 
 // constructor funtion
+// note in naming functiopn of object consturctor is that
+// the firstname should be in capital letter
+// note every object in JS have a constructor fnction,
+// which revers to the property or functio n use in creating the function
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw");
+  };
+}
+
+const another = new Circle(1);
